0181d8acc6bca6e63edb39426d35a41ee06beccd
KarenSmith <karnahada11.11@email.com>
2024-09-22 13:15:26 +0545

    commit message

---
+++ forms/detailscon.php
@@ -0,0 +1,34 @@
+<?php
+// Database credentials
+$servername = "localhost";
+$username = "root"; // Update this to your MySQL username
+$password = "";     // Update this to your MySQL password
+$dbname = "contact_form"; // The database you created
+
+// Create a connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Check if form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $conn->real_escape_string($_POST['name']);
+    $email = $conn->real_escape_string($_POST['email']);
+    $subject = $conn->real_escape_string($_POST['subject']);
+    $message = $conn->real_escape_string($_POST['message']);
+
+    // SQL query to insert form data into the database
+    $sql = "INSERT INTO messages (name, email, subject, message) VALUES ('$name', '$email', '$subject', '$message')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close(); // Close the connection
+}
+?>
